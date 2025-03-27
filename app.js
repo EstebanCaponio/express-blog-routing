52f4dd70bd6e0fa204abfc8075126c79d83c0f3c
EstebanCaponio <estebancaponio@gmail.com>
2025-03-27 15:55:16 +0100

    feat: connected postsArray and posts to app.js

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const express = require('express');
-const posts=require('./posts');
+const posts=require('./postsArray');
+const router=require('./routers/posts');
 const app = express();
 const port = 4000;
+
+// app.use("/postsArray", posts);
+app.use("/posts", router);
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.send('test')
+    res.send('sei nella home');
 })
 
-app.get('/posts', (req, res) => {
-    res.send(posts);
-})
+// app.get('/postsArray', (req, res) => {
+//     res.send(posts);
+// })
+
+
 
 app.listen(port, () => {
     console.log(`hei, Example app listening on port ${port}`)
