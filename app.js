d757f7c46e2627a8902f3eaaf69dea3480bc0121
EstebanCaponio <estebancaponio@gmail.com>
2025-03-27 16:20:42 +0100

    feat: refactoring

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+// collllego express
 const express = require('express');
+// collego postsArray e posts nella cartella router
 const posts=require('./postsArray');
 const router=require('./routers/posts');
 const app = express();
 const port = 4000;
 
-// app.use("/postsArray", posts);
 app.use("/posts", router);
 
 app.use(express.static('public'));
@@ -12,12 +13,6 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.send('sei nella home');
 })
-
-// app.get('/postsArray', (req, res) => {
-//     res.send(posts);
-// })
-
-
 
 app.listen(port, () => {
     console.log(`hei, Example app listening on port ${port}`)
